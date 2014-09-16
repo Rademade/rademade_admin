22c3ef5f488cd2641211713b20caed343ded9e2b
Sergey Kuzhavskiy <kuzh@rademade.com>
2014-09-16 16:56:29 +0300

    added AR localize

--- lib/rademade_admin/model/adapter/data/active_record.rb
+++ lib/rademade_admin/model/adapter/data/active_record.rb
@@ -20,40 +20,8 @@ module RademadeAdmin
 
           def _map_fields
             fields = {}
-            @model.column_types.each do |name, field_data|
-              name = name.to_sym
-              column_data = extract_column_data(field_data)
-              getter = name.to_s
-              fields[name] = RademadeAdmin::Model::Info::Field.new({
-                :name => name,
-                :primary => column_data.primary,
-                :getter => getter,
-                :setter => getter + '=',
-                :type => column_data.type, # review: is it using somewhere?
-                :localizable => false,
-                :relation_name => name[/(.+)_id$/, 1]
-              })
-            end
-            translation_class = @model.try(:translation_class)
-            if translation_class
-              @model.try(:translated_attribute_names).each do |name|
-                fields[name] = RademadeAdmin::Model::Info::Field.new({
-                  :name => name,
-                  :primary => false,
-                  :getter => name,
-                  :setter => name + '=',
-                  :type => :string,
-                  :localizable => true,
-                  :relation_name => nil
-                })
-              end
-              # it is easier to change I18n before setting new value, for Mongoid same
-              # Article::Translation.where(:article_id => a.id, :locale => :en).first
-              # set attr
-              # save
-              binding.pry
-            end
-            fields
+            _add_non_localizable_fields fields
+            _add_localizable_fields fields
           end
 
           def _map_relations
@@ -79,6 +47,41 @@ module RademadeAdmin
               end
             end
             relations
+          end
+
+          def _add_non_localizable_fields(fields)
+            @model.column_types.each do |name, field_data|
+              name = name.to_sym
+              column_data = extract_column_data(field_data)
+              getter = name.to_s
+              fields[name] = RademadeAdmin::Model::Info::Field.new({
+                :name => name,
+                :primary => column_data.primary,
+                :getter => getter,
+                :setter => getter + '=',
+                :type => column_data.type,
+                :localizable => false,
+                :relation_name => name[/(.+)_id$/, 1]
+              })
+            end
+            fields
+          end
+
+          def _add_localizable_fields(fields)
+            @model.try(:translated_attribute_names).each do |name|
+              name = name.to_sym
+              getter = name.to_s
+              fields[name] = RademadeAdmin::Model::Info::Field.new({
+                :name => name,
+                :primary => false,
+                :getter => getter,
+                :setter => getter + '=',
+                :type => :string,
+                :localizable => true,
+                :relation_name => nil
+              })
+            end if @model.try(:translation_class)
+            fields
           end
 
           private
