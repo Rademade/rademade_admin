8ae16cb202f3b52e481fedcbda396edb3e61e619
Sergey Kuzhavskiy <kuzh@rademade.com>
2014-10-15 18:37:14 +0300

    changed permit to slice

--- app/services/saver.rb
+++ app/services/saver.rb
@@ -94,7 +94,7 @@ module RademadeAdmin
     end
 
     def simple_field_params
-      @params.require(:data).permit(@model_info.data_items.save_form_fields)
+      @params.require(:data).symbolize_keys.slice(*@model_info.data_items.save_form_fields)
     end
 
     def related_entities(data_item, ids)
