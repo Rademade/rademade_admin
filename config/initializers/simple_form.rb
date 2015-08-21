fb72c0cd7bc968d6c32f1da8f1dadbbcd6be980d
Sergey Kuzhavskiy <kuzh@rademade.com>
2015-08-21 17:57:47 +0300

    updated form buttons

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -63,9 +63,6 @@ SimpleForm.setup do |config|
   # Default tag used for error notification helper.
   config.error_notification_tag = :div
 
-  # CSS class to add for error notification helper.
-  config.error_notification_class = 'alert alert-error'
-
   # ID to add for error notification helper.
   # config.error_notification_id = nil
 
@@ -91,9 +88,6 @@ SimpleForm.setup do |config|
 
   # How the label text should be generated altogether with the required text.
   # config.label_text = lambda { |label, required| "#{required} #{label}" }
-
-  # You can define the class to use on all labels. Default is nil.
-  config.label_class = 'control-label'
 
   # You can define the class to use on all forms. Default is simple_form.
   # config.form_class = :simple_form
@@ -140,5 +134,7 @@ SimpleForm.setup do |config|
 
   # Default class for inputs
   # config.input_class = nil
+
+  config.button_class = nil
 
 end
