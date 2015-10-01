e5c2883d4ea0cdd0acb08df5393a7e97d70fa741
Sergey Kuzhavskiy <kuzh@rademade.com>
2015-10-01 17:23:20 +0300

    refactored template service

--- app/services/template_service.rb
+++ app/services/template_service.rb
@@ -2,13 +2,6 @@
 module RademadeAdmin
   class TemplateService
 
-    attr_writer :main_layout, :login_layout, :content_item_layout, :layout_head, :search_block,
-                :navigation_block, :navigation_menu_block, :navigation_menu_item_block,
-                :pagination_block, :on_page_switcher_block, :table_head_block, :table_head_list,
-                :list_block, :form_block, :form_control_block, :form_lang_panel_block,
-                :destroy_button, :edit_button, :hide_button, :preview_button, :gallery_button,
-                :cancel_button, :save_and_return_button, :save_button
-
     def initialize(root_dir)
       @root_dir = root_dir
     end
@@ -17,103 +10,45 @@ module RademadeAdmin
       "#{@root_dir}/#{directories.join('/')}"
     end
 
-    def main_layout
-      @main_layout ||= layout_path 'main'
-    end
-
-    def login_layout
-      @login_layout ||= layout_path 'login'
-    end
-
-    def content_item_layout
-      @content_item_layout ||= layout_path 'content_item'
-    end
-
-    def layout_head
-      @layout_head ||= layout_path 'parts/head'
-    end
-
-    def navigation_block
-      @navigation_block ||= block_path 'navigation'
-    end
-
-    def navigation_menu_block
-      @navigation_menu_block ||= block_path 'navigation/menu'
-    end
-
-    def navigation_menu_item_block
-      @navigation_menu_item_block ||= block_path 'navigation/menu/item'
-    end
-
-    def search_block
-      @search_block ||= block_path 'search'
-    end
-
-    def pagination_block
-      @pagination_block ||= block_path 'pagination'
-    end
-
-    def on_page_switcher_block
-      @on_page_switcher_block ||= block_path 'on_page_switcher'
-    end
-
-    def table_head_block
-      @table_head_block ||= block_path 'table/head'
-    end
-
-    def table_head_list
-      @table_head_list ||= block_path 'table/list'
-    end
-
-    def list_block
-      @list_block ||= block_path 'list'
-    end
-
-    def form_block
-      @form_block ||= block_path 'form'
-    end
-
-    def form_control_block
-      @form_control_block ||= block_path 'form/control'
-    end
-
-    def form_lang_panel_block
-      @form_lang_panel_block ||= block_path 'form/lang_panel'
-    end
-
-    def destroy_button
-      @destroy_button ||= block_path 'button/destroy'
-    end
-
-    def edit_button
-      @edit_button ||= block_path 'button/edit'
-    end
-
-    def hide_button
-      @hide_button ||= block_path 'button/hide'
-    end
-
-    def preview_button
-      @preview_button ||= block_path 'button/preview'
-    end
-
-    def gallery_button
-      @gallery_button ||= block_path 'button/gallery'
-    end
-
-    def cancel_button
-      @cancel_button ||= block_path 'button/cancel'
-    end
-
-    def save_and_return_button
-      @save_and_return_button ||= block_path 'button/save_and_return'
-    end
-
-    def save_button
-      @save_button ||= block_path 'button/save'
+    def method_missing(name, *arguments)
+      setter = /(?<name>.*)=/.match(name)
+      if setter
+        template_paths[setter[:name].to_sym] = arguments[0]
+      else
+        template_paths[name]
+      end
     end
 
     private
+
+    def template_paths
+      @template_paths ||= {
+        :main_layout => layout_path('main'),
+        :login_layout => layout_path('login'),
+        :content_item_layout => layout_path('content_item'),
+        :layout_head => layout_path('parts/head'),
+        :navigation_block => block_path('navigation'),
+        :navigation_menu_block => block_path('navigation/menu'),
+        :navigation_menu_item_block => block_path('navigation/menu/item'),
+        :search_block => block_path('search'),
+        :pagination_block => block_path('pagination'),
+        :on_page_switcher_block => block_path('on_page_switcher'),
+        :table_head_block => block_path('table/head'),
+        :table_head_list => block_path('table/list'),
+        :list_block => block_path('list'),
+        :form_block => block_path('form'),
+        :form_control_block => block_path('form/control'),
+        :form_lang_panel_block => block_path('form/lang_panel'),
+        :destroy_button => block_path('button/destroy'),
+        :edit_button => block_path('button/edit'),
+        :hide_button => block_path('button/hide'),
+        :preview_button => block_path('button/preview'),
+        :gallery_button => block_path('button/gallery'),
+        :cancel_button => block_path('button/cancel'),
+        :save_and_return_button => block_path('button/save_and_return'),
+        :save_button => block_path('button/save')
+      }
+    end
 
     def abstract_template(path)
       template_path 'abstract', path
