26a391a53fa0df516c690749b112195bb43180d8
Sergey Kuzhavskiy <kuzh@rademade.com>
2015-10-02 17:07:09 +0300

    added dynamic model infos

--- lib/rademade_admin/model/graph.rb
+++ lib/rademade_admin/model/graph.rb
@@ -4,22 +4,29 @@ module RademadeAdmin
     class Graph
       include Singleton
 
+      DEFAULT_MODULE_NAME = 'RademadeAdmin'
+
       def add_pair(module_name, controller_name, inner)
         # Controller includes configuration for mapping model
         controller = controller_class(module_name, "#{controller_name}_controller")
         controller.configuration.model(controller_name.classify) unless controller.model_name
-
         model = controller.model_class
-
-        unless @model_infos[model.to_s]
-          model_reflection = RademadeAdmin::Model::Reflection.new(model, controller_name, module_name)
-          model_info = RademadeAdmin::Model::Info.new(model_reflection, controller.configuration, inner)
-          @model_infos[model.to_s] = model_info
-        end
+        @model_infos[model.to_s] ||= init_model_info({
+          :model => model,
+          :controller_name => controller_name,
+          :module_name => module_name,
+          :controller => controller,
+          :inner => inner
+        })
       end
 
       def model_info(model)
-        @model_infos[model.to_s]
+        model_name = model.to_s
+        @model_infos[model_name] ||= init_model_info({
+          :model => model,
+          :controller => default_controller(model_name),
+          :inner => false
+        })
       end
 
       def root_models
@@ -32,11 +39,36 @@ module RademadeAdmin
         @model_infos = {}
       end
 
+      def init_model_info(options)
+        model_reflection = RademadeAdmin::Model::Reflection.new(
+          options[:model],
+          options[:controller_name],
+          options[:module_name]
+        )
+        RademadeAdmin::Model::Info.new(model_reflection, options[:controller].configuration, options[:inner])
+      end
+
       def controller_class(module_name, full_controller_name)
         LoaderService.const_get("#{module_name}/#{full_controller_name}")
       rescue
-        dynamic_controller = Class.new(RademadeAdmin::ModelController)
-        LoaderService.const_get(module_name).const_set "#{full_controller_name}".classify, dynamic_controller
+        create_controller(full_controller_name, module_name)
+      end
+
+      def default_controller(model_name)
+        controller = create_controller("#{model_name.pluralize}_controller")
+        controller.configuration.model(model_name)
+        controller
+      end
+
+      def create_controller(controller_name, module_name = nil)
+        controller_module = module_name.nil? ? default_module : LoaderService.const_get(module_name)
+        controller_module.const_set(controller_name.classify, Class.new(RademadeAdmin::ModelController))
+      end
+
+      def default_module
+        LoaderService.const_get(DEFAULT_MODULE_NAME)
+      rescue
+        Object.const_set(DEFAULT_MODULE_NAME, Module.new)
       end
 
     end
